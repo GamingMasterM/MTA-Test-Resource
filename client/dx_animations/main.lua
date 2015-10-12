b84a39f7076fed42b59b465332104ae975a80866
GamingMasterM <masterm4yt@gmail.com>
2015-10-12 10:49:18 +0200

    Scripttutorial #23 - animieren und rendern

--- client/dx_animations/main.lua
+++ client/dx_animations/main.lua
@@ -1,12 +1,30 @@
 if isTestable("dx_animations") then
+  setFPSLimit(25)
+  local bActive = false
+  local iX = 0
+  local iFPS = 40
 
 
-  local function test1()
-    _debug("Helau!")
+  local function render()
+    local iEasing = getEasingValue(iX/scx,"InOutQuad")
+    dxDrawText(getFPS(),scx/2, scy/2,scx/2, scy/2)
+    dxDrawImage(0,0,iEasing*scx,scy/2,"res/img/logo_512.png")
+    if iX >= scx then
+      iX = scx
+    else
+      iX = iX + 10*iFPS/getFPS()
+    end
   end
-  test1()
 
+  local function toggle(bToggle)
+    if bToggle and not bActive then
+      addEventHandler("onClientRender", root, render)
+      bActive = true
+    elseif not bToggle and bActive then
+      removeEventHandler("onClientRender", root, render)
+      bActive = false
+    end
+  end
 
-
-
+  toggle(true)
 end
--- client/main.lua
+++ client/main.lua
@@ -10,3 +10,30 @@
 
 lp = getLocalPlayer()
 scx, scy = guiGetScreenSize()
+
+
+
+
+
+--//
+--||  getFPS by Krischkros
+--\\
+
+local fps_counter = 0
+local fps = getFPSLimit()
+
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	setTimer(calculate_fps, 1000, 0)
+end)
+addEventHandler("onClientRender", root, function()
+	fps_counter = fps_counter + 1
+end)
+function calculate_fps()
+	fps = fps_counter
+	fps_counter = 0
+
+end
+
+function getFPS()
+	return fps
+end
